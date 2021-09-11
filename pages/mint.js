eb741d7e24ecb2f081b6fe058b8b6061a71ed491
Deon Roos <44850848+DotDeon@users.noreply.github.com>
2021-09-11 11:40:53 +0200

    Mint Page Mobile

--- pages/mint.js
+++ pages/mint.js
@@ -1,21 +1,14 @@
 import Head from "next/head";
-import About from "../components/about";
 import Banner from "../components/banner";
-import ConnectWallet from "../components/connectwallet";
-import Copyright from "../components/copyright";
-import Countdown from "../components/countdown";
 import Faqs from "../components/faqs";
-import Footer from "../components/footer";
-import Header from "../components/header";
-import Mint from "../components/mintbutton";
 import Roadmap from "../components/roadmap";
 import Sidebar from "../components/sidemenu";
-import Story from "../components/story";
-export default function mint() {
+
+export default function Mint() {
   return (
-    <div className="absolute  w-screen bg-black h-screen">
+    <div className="bg-black w-screen h-screen">
       <Head>
-        <title>Mint Solanoid</title>
+        <title>solanoid</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
@@ -24,10 +17,10 @@ export default function mint() {
           rel="stylesheet"
         />
       </Head>
-      <div className="flex flex-row w-screen bg-black">
+      <div className="flex flex-row min-w-max min-h-screen bg-black">
         <Sidebar />
-        <div className="flex flex-col pt-6 ml-2 w-full bg-black">
-          <div className="flex-none md:flex-1 mx-auto scrollbar-hide overflow-y-scroll ">
+        <div className="flex flex-col ml-2 bg-black w-screen">
+          <div className="flex-none md:flex-1 bg-black mx-auto scrollbar-hide overflow-y-scroll ">
             <ConnectWallet />
             <div className="flex-col h-1/2">
               <Countdown />
