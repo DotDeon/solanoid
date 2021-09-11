1446866532d0ecb74cdc3f58b3a15fb7270e5b8d
Deon Roos <44850848+DotDeon@users.noreply.github.com>
2021-09-11 13:44:38 +0200

    faqs

--- components/faqs.js
+++ components/faqs.js
@@ -54,8 +54,8 @@ function Faqs() {
             </div>
             <div className="flex flex-col flex-grow">
               <div className="items-center justify-between">
-                <div className="flex flex-row ml-8 mt-2">
-                  <p className="text-white ml-4 text-4xl md:text-6xl  font-Raleway font-semibold">
+                <div className="flex flex-row ml-8 mt-4 md:mt-2">
+                  <p className="text-white ml-4 mt-2 text-4xl md:text-6xl  font-Raleway font-semibold">
                     what is the total supply?
                   </p>
                 </div>
@@ -89,8 +89,8 @@ function Faqs() {
             </div>
             <div className="flex flex-col flex-grow">
               <div className="items-center justify-between">
-                <div className="flex flex-row ml-8 mt-2">
-                  <p className="text-white ml-4 text-4xl md:text-6xl  font-Raleway font-semibold">
+                <div className="flex flex-row ml-8 mt-4 md:mt-2">
+                  <p className="text-white ml-4 mt-2 text-4xl md:text-6xl  font-Raleway font-semibold">
                     how much does it cost to mint?
                   </p>
                 </div>
@@ -124,8 +124,8 @@ function Faqs() {
             </div>
             <div className="flex flex-col flex-grow">
               <div className="items-center justify-between">
-                <div className="flex flex-row ml-8 mt-2">
-                  <p className="text-white ml-4 text-4xl md:text-6xl font-Raleway font-semibold">
+                <div className="flex flex-row ml-8 mt-4 md:mt-2">
+                  <p className="text-white ml-4 mt-2 text-4xl md:text-6xl font-Raleway font-semibold">
                     when is the launch?
                   </p>
                 </div>
@@ -159,8 +159,8 @@ function Faqs() {
             </div>
             <div className="flex flex-col flex-grow">
               <div className="items-center justify-between">
-                <div className="flex flex-row ml-8 mt-2">
-                  <p className="text-white ml-4 text-4xl md:text-6xl  font-Raleway font-semibold">
+                <div className="flex flex-row ml-8 mt-4 md:mt-2">
+                  <p className="text-white ml-4 mt-2 text-4xl md:text-6xl  font-Raleway font-semibold">
                     will there be a presale?
                   </p>
                 </div>
@@ -194,8 +194,8 @@ function Faqs() {
             </div>
             <div className="flex flex-col flex-grow">
               <div className="items-center justify-between">
-                <div className="flex flex-row ml-8 mt-2">
-                  <p className="text-white ml-4 text-4xl md:text-6xl  font-Raleway font-semibold">
+                <div className="flex flex-row ml-8 mt-4 md:mt-2">
+                  <p className="text-white ml-4 mt-2 text-4xl md:text-6xl  font-Raleway font-semibold">
                     are there giveaways?
                   </p>
                 </div>
@@ -229,8 +229,8 @@ function Faqs() {
             </div>
             <div className="flex flex-col flex-grow">
               <div className="items-center justify-between">
-                <div className="flex flex-row ml-8 mt-3">
-                  <p className="text-white ml-2 text-4xl md:text-5xl  font-Raleway font-semibold">
+                <div className="flex flex-row ml-8 mt-4 md:mt-2">
+                  <p className="text-white ml-2 mt-2 text-4xl md:text-5xl  font-Raleway font-semibold">
                     what will minting funds go towards?
                   </p>
                 </div>
