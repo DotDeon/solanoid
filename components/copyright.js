f10c0f58c4b04454b47f02c0f9774717afe109ef
Deon Roos <44850848+DotDeon@users.noreply.github.com>
2021-09-12 10:48:19 +0200

    change text size for copyright mobile

--- components/copyright.js
+++ components/copyright.js
@@ -5,7 +5,7 @@ import logo from "../assets/logo.svg";
 function Copyright() {
   return (
     <div className="flex flex-row bg-white w-screen h-20 items-center justify-center">
-      <p className="text-black font-semibold text-lg font-Raleway">
+      <p className="text-black font-semibold text-base md:text-1xl font-Raleway">
         Copyright @ 2021 Solanoid. All Rights Reserved.
       </p>
     </div>
