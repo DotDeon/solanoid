1c48d27e5b754df336dd726bab5ee7bdfe46c95f
Deon Roos <44850848+DotDeon@users.noreply.github.com>
2021-09-12 15:48:25 +0200

    fixed Connect Wallet

--- components/connectwallet.js
+++ components/connectwallet.js
@@ -3,7 +3,7 @@ import logo from "../assets/logo.svg";
 
 function ConnectWallet({ placeholder }) {
   return (
-    <header className="sticky flex justify-end mt-4 pt-2 pr-6 w-screen top-0 z-50">
+    <header className="flex justify-end mt-4 pt-2 mr-2 md:pr-6 w-screen top-0 z-50">
       <button className=" bg-gradient-to-b from-white to-gradblue hover:bg-blue-700 text-black font-bold mr-16 py-3 px-12 rounded-full">
         Connect Wallet
       </button>
